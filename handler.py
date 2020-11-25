6e6b80e97764951ed983960feaaaeafe8f2c02f7
mytalk <qiudong.1024@gmail.com>
2020-11-25 18:08:22 +0800

    建表

--- handler.py
+++ handler.py
@@ -5,9 +5,12 @@ from tornado import gen, web
 import logging as log
 import json
 from common import JsonCustomEncoder, DBTool
+from model import Person, Config
 
 db = DBTool()
 
+
+# 查询人员
 def find_persons(status, first_prize, second_prize):
     sql = 'select * from test where name = ? and age = ?'
     temp = db.query(sql, ['name', 12])
@@ -15,12 +18,39 @@ def find_persons(status, first_prize, second_prize):
         print('ID:', st[0], '  Name:', st[1], '  Age:', st[2])
 
 
+# 查询配置
+def find_config(param_key):
+    sql = 'select * from CONFIG where PARAM_KEY = ?'
+    result = db.query(sql, [param_key])
+    configs = []
+    for r in result:
+        configs.append(Config(r[0], r[1]))
+    return configs
+
+
+configs = find_config('lottery_type_order')
+
+for config  in configs:
+    print(str(config))
+
 # 获取配置
 class HomeConfig(web.RequestHandler):
     @gen.coroutine
     def get(self):
         log.info('获取配置')
-        response = []
+
+
+        response = {
+            'code':'0000',
+            'desc':'交易成功',
+            'lottery_type_order': lottery_type_order,
+            'prize1_total_num': prize1_total_num,
+            'prize1_take_count': prize1_take_count,
+            'prize2_total_num': prize2_total_num,
+            'prize2_take_count': prize2_take_count,
+            'prize3_total_num': prize3_total_num,
+            'prize4_take_count': prize3_take_count
+        }
         self.write(json.dumps(response, cls=JsonCustomEncoder))
 
 
--- model.py
+++ model.py
@@ -5,8 +5,6 @@
 class Person(object):
 
     def __init__(self):
-        # 手机号
-        self.mobile = None
         # 姓名
         self.name = None
         # 头像
@@ -18,8 +16,7 @@ class Person(object):
         # 二等奖优先标记 Y
         self.second_prize = None
 
-    def init(self, mobile, name, pic, status, first_prize, second_prize):
-        self.mobile = mobile
+    def init(self, name, pic, status, first_prize, second_prize):
         self.name = name
         self.pic = pic
         self.status = status
@@ -29,9 +26,12 @@ class Person(object):
 
 # 配置信息
 # param_key: param_object
-# 抽奖顺序(默认) order: s1(special 1等奖),s2(special 2等奖),3(3等奖),2(2等奖),1(1等奖)
-# 抽奖配置 prize1:k,m,n  prize2:k,m,n  prize3:k,m,n
+# 抽奖顺序(默认) lottery_type_order: s1(special 1等奖),s2(special 2等奖),3(3等奖),2(2等奖),1(1等奖)
+# 抽奖配置 prize1_total_num prize1_take_count prize2_total_num prize2_take_count prize3_total_num prize3_take_count
 class Config(object):
-    def __init__(self):
-        self.param_key = None
-        self.param_object = None
+    def __init__(self, param_key, param_object):
+        self.param_key = param_key
+        self.param_object = param_object
+
+    def __str__(self) :
+        return 'param_key:' + self.param_key + ',' + 'param_object:' + self.param_object
