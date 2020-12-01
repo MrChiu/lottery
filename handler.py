ae9eeff70c39938e2839bc83e837772bdd502e69
mytalk <qiudong.1024@gmail.com>
2020-12-01 19:27:52 +0800

    。

--- handler.py
+++ handler.py
@@ -120,31 +120,37 @@ class LotteryStop(web.RequestHandler):
 # 抽奖设置
 class LotterySetting(web.RequestHandler):
     @gen.coroutine
-    def get(self):
+    def post(self):
         log.info('抽奖设置')
         try:
-            lottery_type_order = self.get_argument('lottery_type_order', default='s1,s2,3,2,1,0')
-            prize0_total_num = self.get_argument('prize0_total_num', default=0)
-            prize0_take_count = self.get_argument('prize0_take_count', default=0)
-            prize1_total_num = self.get_argument('prize1_total_num', default=0)
-            prize1_take_count = self.get_argument('prize1_take_count', default=0)
-            prize2_total_num = self.get_argument('prize2_total_num', default=0)
-            prize2_take_count = self.get_argument('prize2_take_count', default=0)
-            prize3_total_num = self.get_argument('prize3_total_num', default=0)
-            prize3_take_count = self.get_argument('prize3_take_count', default=0)
-            special_prize1_person = self.get_argument('special_prize1_person')
-            special_prize2_person = self.get_argument('special_prize2_person')
-            update_config_val('lottery_type_order', lottery_type_order)
-            update_config_val('prize0_total_num', prize0_total_num)
-            update_config_val('prize0_take_count', prize0_take_count)
-            update_config_val('prize1_total_num', prize1_total_num)
-            update_config_val('prize1_take_count', prize1_take_count)
-            update_config_val('prize2_total_num', prize2_total_num)
-            update_config_val('prize2_take_count', prize2_take_count)
-            update_config_val('prize3_total_num', prize3_total_num)
-            update_config_val('prize3_take_count', prize3_take_count)
-            update_config_val('special_prize1_person', special_prize1_person)
-            update_config_val('special_prize2_person', special_prize2_person)
+            post_data = self.request.body_arguments
+            post_data = {x: post_data.get(x)[0].decode("utf-8") for x in post_data.keys()}
+            if not post_data:
+                post_data = self.request.body.decode('utf-8')
+                post_data = json.loads(post_data)
+            print(post_data)
+            # lottery_type_order = self.get_argument('lottery_type_order', default='s1,s2,3,2,1,0')
+            # prize0_total_num = self.get_argument('prize0_total_num', default=0)
+            # prize0_take_count = self.get_argument('prize0_take_count', default=0)
+            # prize1_total_num = self.get_argument('prize1_total_num', default=0)
+            # prize1_take_count = self.get_argument('prize1_take_count', default=0)
+            # prize2_total_num = self.get_argument('prize2_total_num', default=0)
+            # prize2_take_count = self.get_argument('prize2_take_count', default=0)
+            # prize3_total_num = self.get_argument('prize3_total_num', default=0)
+            # prize3_take_count = self.get_argument('prize3_take_count', default=0)
+            # special_prize1_person = self.get_argument('special_prize1_person')
+            # special_prize2_person = self.get_argument('special_prize2_person')
+            # update_config_val('lottery_type_order', lottery_type_order)
+            # update_config_val('prize0_total_num', prize0_total_num)
+            # update_config_val('prize0_take_count', prize0_take_count)
+            # update_config_val('prize1_total_num', prize1_total_num)
+            # update_config_val('prize1_take_count', prize1_take_count)
+            # update_config_val('prize2_total_num', prize2_total_num)
+            # update_config_val('prize2_take_count', prize2_take_count)
+            # update_config_val('prize3_total_num', prize3_total_num)
+            # update_config_val('prize3_take_count', prize3_take_count)
+            # update_config_val('special_prize1_person', special_prize1_person)
+            # update_config_val('special_prize2_person', special_prize2_person)
             response = {
                 'code': '0000',
                 'desc': '交易成功'
